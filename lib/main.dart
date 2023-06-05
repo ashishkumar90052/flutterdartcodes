eeb38af9cee4de211e291baf4b1583cf337a03bd
ashishkumar90052 <itsashishkumar.personal@gmail.com>
2023-06-05 15:26:12 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,33 @@
-import 'package:dynamicapp/allscreens/23.share_plus.dart';
-import 'package:dynamicapp/allscreens/25.list_img_color.dart';
+import 'package:dynamicapp/allscreens/28.crypto.dart';
 import 'package:flutter/material.dart';
 
-import 'allscreens/24.animated_theme_switcher.dart';
-
 void main() {
-  runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.dark,
-        theme: ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.light,
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.dark,
-        ),
-        // darkTheme: ThemeData.dark(useMaterial3: true),
-        // theme: ThemeData.dark(
-        //   useMaterial3: true,
-        //   // colorScheme: const ColorScheme.dark(),
-        // )
-        // .copyWith(
-        //   pageTransitionsTheme: const PageTransitionsTheme(
-        //     builders: <TargetPlatform, PageTransitionsBuilder>{
-        //       TargetPlatform.android: ZoomPageTransitionsBuilder(),
-        //     },
-        // ),
-        // ),
-        home: const CardImgColor()),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        // textTheme: GoogleFonts.notoSansTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        // textTheme: GoogleFonts.notoSansTextTheme(),
+        brightness: Brightness.dark,
+      ),
+
+      // darkTheme: ThemeData.dark(useMaterial3: true),
+      // theme: ThemeData.dark(
+      //   useMaterial3: true,
+      //   // colorScheme: const ColorScheme.dark(),
+      // )
+      // .copyWith(
+      //   pageTransitionsTheme: const PageTransitionsTheme(
+      //     builders: <TargetPlatform, PageTransitionsBuilder>{
+      //       TargetPlatform.android: ZoomPageTransitionsBuilder(),
+      //     },
+      // ),
+      // ),
+      home: const Crypto()));
 }
